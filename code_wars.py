ce45c9a4b3e42577d40664d55ff2a6669cff9a26
Rennikh <daniil19925@gmail.com>
2022-08-17 23:55:03 +0600

    Add function hello_world

---
+++ code_wars.py
@@ -0,0 +1,4 @@
+def hello_world(phrase) -> str:
+    """Function return any phrase"""
+
+    return str(phrase)
---
+++ main.py
@@ -0,0 +1,5 @@
+from code_wars import hello_world
+
+
+if __name__ == '__main__':
+    print(hello_world("Hello world!"))
